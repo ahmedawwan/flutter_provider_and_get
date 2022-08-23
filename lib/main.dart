0e664702a0c4c70caa0327777046804e802563c2
Ahmed Saeed <ahmed.saeed@ropstam.com>
2022-08-23 12:21:20 +0500

    Files ordered

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: const HomePage(),),
+          home: const HomePage(),
+      ),
     );
   }
 }
