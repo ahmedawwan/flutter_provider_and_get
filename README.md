01b413d24a38faebe09e6ee93136e897f2dc7d0f
Ahmed Saeed <ahmed.saeed@ropstam.com>
2022-08-23 12:09:57 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter provider and getx
+
+Simple application to understand navigation and snackbars with get package and state management with provider package further more
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_flutter_2/data_class.dart';
+import 'package:get/get.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => DataClass(),
+      child: GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomePage(),),
+    );
+  }
+}
